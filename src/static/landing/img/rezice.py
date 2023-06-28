93a79f0aeed7316c52c222ff6e7e492efc62f82c
driendro <jorge.ronconi@gmail.com>
2023-06-28 15:19:20 -0300

    actulizacion de infromacion en la pagina

--- src/static/landing/img/rezice.py
+++ src/static/landing/img/rezice.py
@@ -1,10 +1,14 @@
 from PIL import Image
-razon=1.2
-imagen = Image.open('src\static\landing\img\logo_ingreso_origin.png')
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+razon=1/2
+imagen = Image.open(BASE_DIR / 'banner.png')
 tamano_destino = (int(imagen.size[0]*razon) ,int(imagen.size[1]*razon))
 
-x='logo_ingreso'
-imagen = Image.open('src\static\landing\img\{}_origin.png'.format(x))
-#imagen.save('src\static\landing\img\{}_origin.png'.format(x))
+x='banner'
+imagen = Image.open(BASE_DIR / '{}.png'.format(x))
+imagen.save('src\static\landing\img\{}_origin.png'.format(x))
 reduccion = imagen.resize(tamano_destino, Image.ANTIALIAS)
-reduccion.save('src\static\landing\img\{}.png'.format(x))
+reduccion.save(BASE_DIR / '{}.png'.format(x))
