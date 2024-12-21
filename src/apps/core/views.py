e32ac41d3a540721a44f3915769d5d5bbe412922
Fermat <jorge.ronconi@gmail.com>
2024-12-21 19:44:15 -0300

    update
    El formualrio de inscripcionse habre o cierra en funcion de los cursos disponibles.

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -1,6 +1,11 @@
+# Librerias Standars
+from datetime import date
+
 from django.shortcuts import render
 from django.views.defaults import page_not_found, server_error
 #from django.template import RequestContext
+
+from apps.inscripcion.models import Curso
 
 # Create your views here.
 
@@ -8,7 +13,20 @@ from django.views.defaults import page_not_found, server_error
 def landing_page(request):
     request.session['dni_verificado'] = False
     request.session['credencial'] = None
-    return render(request, 'landing/index.html')
+    hoy = date.today()  # hoy
+    proximo_curso = Curso.objects.filter(
+        inscripcion_inicio__gte=hoy).order_by('inscripcion_inicio').first()
+    ultimo_curso = Curso.objects.filter(
+        inscripcion_cierre__lte=hoy).order_by('inscripcion_cierre').last()
+    context={}
+    if proximo_curso:
+        context['inicio_inscripcion'] = proximo_curso.inscripcion_inicio
+        context['final_inscripcion'] = proximo_curso.inscripcion_cierre
+    else:
+        context['inicio_inscripcion'] = False
+        context['final_inscripcion'] = ultimo_curso.inscripcion_cierre
+
+    return render(request, 'landing/index.html', context=context)
 
 # Error 404
 def Error_404(request, exception):
